8a46af46c5b2cebea31e3723eb5073df4bdf37cf
Stas <stasspace12@gmail.com>
2022-05-19 14:57:32 +0300

    points class

--- script.js
+++ script.js
@@ -23,14 +23,23 @@ var t0=new Date();
 t0=t0.getTime();
 var t1;
 t1=t0+T;//крайние точки графа
-var dtFrame;//период между отрисовкой кадров 
-var pointsT=[];//времена
-var pointsV=[];//значения
-var glcordX;//корды по x для графа
-var startTime=new Date();
-var timeIterator=0;
+//var pointsT=[];//времена
+//var pointsV=[];//значения
+//var timeIterator=0;
+let points = new Array();
+
+class Point {
+    constructor(params={}) {
+        this.date = params.date || new Date();//времена
+        this.value = params.value || 0;//значения
+    }
+};
 //input
 //input T
+document.querySelector("#inT").addEventListener("change", (e)=>{
+    console.log(e.value);
+});
+
 document.forms.inT.onsubmit = function(){
     var message = this.message.value;
     if(isNaN(message)==false){
@@ -72,10 +81,20 @@ document.forms.inwst.onsubmit = function(){
 function getRandomInt(max) {
     return Math.floor(Math.random() * max * 100)/100;
 }
+
+
+
 function WS(){
-    pointsT[timeIterator]=new Date();
-    pointsV[timeIterator]=getRandomInt(100);
-    timeIterator++;
+    //pointsT[timeIterator]=new Date();
+    //pointsV[timeIterator]=getRandomInt(100);
+
+    points.push(new Point({
+        date: new Date(),
+        value: getRandomInt(100)
+    }));
+
+    //timeIterator++;
+    //console.log(points.date);
     setTimeout(WS,wst);
 }
 WS();
@@ -102,33 +121,49 @@ function xLinesDraw(){
 function yLinesDraw(lt0,lt1){
     var i0=0;
     var i1=0;
-    for(let i=0;i<pointsT.length-1;i++){
-        if((lt0>=pointsT[i])&&(lt0<=pointsT[i+1])){
+    for(let i=0;i<points.length-1;i++){
+
+        let pi = points[i],
+            piNext = points[i+1];
+
+        if((lt0>=pi.date)&&(lt0<=piNext.date)){
+            i0=i;
+        }
+        if((lt1>=pi.date)&&(lt1<=piNext.date)){
+            i1=i+1;
+        }
+
+        /*if((lt0>=pointsT[i])&&(lt0<=pointsT[i+1])){
             i0=i;
         }
         if((lt1>=pointsT[i])&&(lt1<=pointsT[i+1])){
             i1=i+1;
-        }
+        }*/
     }
+
     var lx0;
     gctx.beginPath();
     gctx.strokeStyle="#C0C0C0";
     gctx.lineWidth=1;
-    for(let i=i0;i<i1+1;i++){
-        lx0=(pointsT[i]-lt0)*1000*gcordX/(lt1-lt0);
+    for(let i=i0;i<i1;i++){
+
+        lx0=(points[i].date-lt0)*1000*gcordX/(lt1-lt0);
+
+        //lx0=(pointsT[i]-lt0)*1000*gcordX/(lt1-lt0);
         gctx.moveTo(lx0,1000*gcordY);
         gctx.lineTo(lx0,0);
     }
     gctx.stroke();
 }
+
 function grafLineDraw(lt0,lt1){
     var i0=0;
     var i1=0;
-    for(let i=0;i<pointsT.length-1;i++){
-        if((lt0>=pointsT[i])&&(lt0<=pointsT[i+1])){
+    for(let i=0;i<points.length-1;i++){
+        if((lt0>=points[i].date)&&(lt0<=points[i+1].date)){
             i0=i;
         }
-        if((lt1>=pointsT[i])&&(lt1<=pointsT[i+1])){
+        if((lt1>=points[i].date)&&(lt1<=points[i+1].date)){
             i1=i+1;
         }
     }
@@ -140,48 +175,20 @@ function grafLineDraw(lt0,lt1){
     gctx.strokeStyle="black";
     gctx.lineWidth=2;
     gctx.font="15px Verdana";
-    for(let i=i0;i<i1+1;i++){
-        lx0=(pointsT[i]-lt0)*1000*gcordX/(lt1-lt0);
-        lY0=(1000-10*pointsV[i])*gcordY;
-        lx1=(pointsT[i+1]-lt0)*1000*gcordX/(lt1-lt0);
-        lY1=(1000-10*pointsV[i+1])*gcordY;
-        gctx.strokeText("[ "+pointsV[i]+" ]",lx0,lY0);
+    for(let i=i0;i<i1;i++){
+        lx0=(points[i].date-lt0)*1000*gcordX/(lt1-lt0);
+        lY0=(1000-10*points[i].value)*gcordY;
+        lx1=(points[i+1].date-lt0)*1000*gcordX/(lt1-lt0);
+        lY1=(1000-10*points[i+1].value)*gcordY;
+        gctx.strokeText("[ "+points[i].value+" ]",lx0,lY0);
         gctx.moveTo(lx0,lY0);
         gctx.lineTo(lx1,lY1);
     }
     gctx.stroke();
 }
 
-//setGraf
-function setGraf(){
-    var i=0;
-    
-    //point counters
-    var r0=i;
-    var r1=i;
-    //graf basis
-    
-    while(t1<pointsT[i]){
-        r1++;
-        i++;
-    }
-    i=r0;
-    //graf values lines
-    gctx.beginPath();
-    gctx.strokeStyle="#C0C0C0";
-    gctx.lineWidth=1;
-    gctx.moveTo(0,pointsV[i]*10*gcordY);
-    i++;
-    for(let j=1;j<=r1-r0;j++){
-        gctx.lineTo(0+j*1000*gcordX/(r1-r0),pointsV[i]*10*gcordY);
-        i++;
-    }
-}
-//set
-function set(){
-
-}
 //launcher
+
 function grafDraw(){
     gctx.clearRect(0, 0, graf.width, graf.height);
     xLinesDraw();
